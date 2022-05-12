1b56a622c9edc10a0401fe107523dd9cd9d7c8ae
Paul Melton <melton.paul@hotmail.com>
2022-05-12 16:01:23 -0500

    React App Started, Welcome screen built.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Start from "./components/Start"
 
-function App() {
+
+export default function App(){
+  const [quizStarted, setQuizStarted] = React.useState(false)
+  
+  function startQuiz(){
+    setQuizStarted(true)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+    {!quizStarted && <Start startQuiz={startQuiz} />}
+      <h1>Test</h1>
 
-export default App;
+    </div>
+  )
+}
