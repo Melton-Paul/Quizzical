16bff31a930d44d8a62266ef7a2dcc68d7cd680b
Paul Melton <melton.paul@hotmail.com>
2022-05-12 18:12:37 -0500

    Built answer randomizer for API call

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ export default function App(){
     setQuizStarted(true)
   }
   React.useEffect(()=> {
-    fetch("https://opentdb.com/api.php?amount=5")
+    fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then(res => res.json())
       .then(data => {
         setQuestions(data.results)
       })
-  }, [])
+  }, [quizStarted])
 
 
   const questionsHtml = questions.map(item => {
-    return <Question question={item.question} />
+    return <Question question={item.question} incorrect={item.incorrect_answers} correct={item.correct_answer} />
   })
 
   return (
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,19 @@
 export default function Question(props){
+    const answersArr= [...props.incorrect, props.correct]
+    const randomizedArr= []
+    for(var i = answersArr.length-1; i >= 0; i--){
+        randomizedArr.push(answersArr.splice(Math.floor(Math.random()*answersArr.length), 1))
+
+      }
+    const answerHtml = randomizedArr.map(item => {
+        return <button className="btn btn--primary answer">{item}</button>
+    })
+
     return (
         <div className="question">
             <h2 className="question__title">{props.question}</h2>
             <div className="question--answers">
-                <button className="btn btn--primary answer">ANSWER</button>
-                <button className="btn btn--primary answer">ANSWER</button>
-                <button className="btn btn--primary answer">ANSWER</button>
-                <button className="btn btn--primary answer">ANSWER</button>
+                {answerHtml}
             </div>
             <hr/>
         </div>
