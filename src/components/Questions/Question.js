8511a30470e455da0229c143a841f278b6116cf6
Paul Melton <melton.paul@hotmail.com>
2022-07-20 18:05:49 -0500

    Non-answer logic, try again rendering.

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -1,37 +1,47 @@
 import React, { useEffect } from "react";
 
 export default function Question(props) {
-  const [answers, setAnswers] = React.useState(props.randomizedArr);
-  const [answered, setAnswered] = React.useState(0);
-  const { quizOver, setPoints, correct } = props;
+  const [answers, setAnswers] = React.useState([]);
+  const { quizOver, setPoints, correct, incorrect_answers, correct_answer } =
+    props;
+  const [answer, setAnswer] = React.useState("");
 
-  function handleChange(id) {
-    setAnswers((prev) => {
-      return prev.map((item) => {
-        return item.id === id
-          ? { ...item, clicked: true }
-          : { ...item, clicked: false };
-      });
-    });
-    setAnswered((prev) => prev + 1);
-    if (answered === 0) {
-      props.setAnsweredQuestions((prev) => prev + 1);
+  useEffect(() => {
+    console.log("Randomizer Ran");
+    const arr = [...incorrect_answers, correct_answer];
+    setAnswers([]);
+
+    for (var i = arr.length - 1; i >= 0; i--) {
+      const id = i;
+      const answer = arr.splice(Math.floor(Math.random() * arr.length), 1);
+      setAnswers((prev) => [...prev, { answer: answer, id: id }]);
     }
+  }, [incorrect_answers, correct_answer]);
+
+  console.log(answers);
+
+  function handleChange(e) {
+    setAnswer(e.target.value);
   }
 
   useEffect(() => {
-    const selected = answers.filter((item) => item.clicked);
-    if (quizOver) {
-      const ans = selected[0].answer[0];
-      const points = ans === correct ? 1 : 0;
-      setPoints((prev) => prev + points);
+    if (!quizOver) {
+      return;
     }
-  }, [quizOver, answers, setPoints, correct]);
+    if (!answer) {
+      return;
+    }
+    const selected = answers.filter((item) => item.answer[0] === answer);
+    const ans = selected[0].answer[0];
+    if (ans === correct) {
+      setPoints((prev) => (prev = prev + 1));
+    }
+  }, [quizOver, answers, correct, setPoints, answer]);
 
   const answerHtml = answers.map((item) => {
     let styles;
     if (props.quizOver) {
-      if (item.clicked) {
+      if (item.answer[0] === answer) {
         styles = {
           backgroundColor:
             item.answer[0] !== props.correct ? "pink" : "#1BE46F",
@@ -48,19 +58,20 @@ export default function Question(props) {
       }
     } else {
       styles = {
-        backgroundColor: item.clicked ? "#4d5b9e" : "white",
-        color: item.clicked ? "white" : "#4d5b9e",
+        backgroundColor: item.answer[0] === answer ? "#4d5b9e" : "white",
+        color: item.answer[0] === answer ? "white" : "#4d5b9e",
       };
     }
+
     return (
       <label className="answer" style={styles} key={item.id}>
         <input
           type="radio"
+          value={item.answer[0]}
+          checked={answer === item.answer[0]}
           className="btn btn--primary"
           name={props.id}
-          onChange={
-            props.quizOver === false ? () => handleChange(item.id) : () => {}
-          }
+          onChange={handleChange}
         />
         {item.answer}
       </label>
@@ -71,6 +82,9 @@ export default function Question(props) {
     <div className="question">
       <h2 className="question__title">{props.question}</h2>
       <div className="question--answers">{answerHtml}</div>
+      {quizOver && !answer && (
+        <p>You did not answer this question, automatically scored a zero.</p>
+      )}
       <hr />
     </div>
   );
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -11,86 +11,96 @@ export default function Questions(props) {
   const [questions, setQuestions] = React.useState([]);
   const [points, setPoints] = React.useState(0);
   const [quizOver, setQuizOver] = React.useState(false);
-  const [answeredQuestions, setAnsweredQuestions] = React.useState(0);
+  const [quizReset, setQuizReset] = React.useState(false);
+  const [questionsHtml, setQuestionsHtml] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     fetch("https://opentdb.com/api.php?amount=5&type=multiple")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.results);
         let fixedIncorrect;
         const fixedHtml = data.results.map((item) => {
           fixedIncorrect = item.incorrect_answers.map((answer) => {
             return decodeHtml(answer);
           });
           return {
-            ...item,
             question: decodeHtml(item.question),
             correct_answer: decodeHtml(item.correct_answer),
             incorrect_answers: fixedIncorrect,
           };
         });
         setQuestions(fixedHtml);
+        setIsLoading(false);
       });
-  }, []);
+  }, [quizReset]);
+
+  React.useEffect(() => {
+    setQuizReset(false);
+    setQuestionsHtml(
+      questions.map((item) => {
+        return (
+          <Question
+            question={item.question}
+            quizOver={quizOver}
+            incorrect_answers={item.incorrect_answers}
+            correct_answer={item.correct_answer}
+            correct={item.correct_answer}
+            setPoints={setPoints}
+            id={"question" + questions.indexOf(item)}
+            key={questions.indexOf(item)}
+          />
+        );
+      })
+    );
+  }, [questions, quizOver]);
 
   function finishQuiz() {
-    if (answeredQuestions === 5) {
-      setQuizOver(true);
-    }
+    setQuizOver(true);
   }
 
-  const questionsHtml = questions.map((item) => {
-    const answersArr = [...item.incorrect_answers, item.correct_answer];
-    const randomizedArr = [];
-    for (var i = answersArr.length - 1; i >= 0; i--) {
-      const answer = answersArr.splice(
-        Math.floor(Math.random() * answersArr.length),
-        1
-      );
-      randomizedArr.push({ answer: answer, clicked: false, id: i });
-    }
-    return (
-      <Question
-        question={item.question}
-        quizOver={quizOver}
-        randomizedArr={randomizedArr}
-        correct={item.correct_answer}
-        setPoints={setPoints}
-        setAnsweredQuestions={setAnsweredQuestions}
-        id={"question" + questions.indexOf(item)}
-        key={questions.indexOf(item)}
-      />
-    );
-  });
+  function resetQuiz() {
+    setPoints(0);
+    setQuizOver(false);
+    setQuizReset(true);
+  }
 
   return (
-    <form className="questions__container">
-      <div className="title">
-        <h1>Quizzical</h1>
-        <div className="title--line"></div>
-      </div>
-      {questionsHtml}
-      {!quizOver ? (
-        <button
-          type="button"
-          className="btn btn--secondary submit"
-          onClick={finishQuiz}
-        >
-          Submit Answers
-        </button>
+    <>
+      {!isLoading ? (
+        <form className="questions__container">
+          <div className="title">
+            <h1>Quizzical</h1>
+            <div className="title--line"></div>
+          </div>
+          {questionsHtml}
+          {!quizOver ? (
+            <button
+              type="button"
+              className="btn btn--secondary submit"
+              onClick={finishQuiz}
+            >
+              Submit Answers
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="btn btn--secondary submit"
+              onClick={resetQuiz}
+            >
+              Try Again
+            </button>
+          )}
+          {quizOver && (
+            <div className="score">
+              You scored {points} points out of 5 points!
+            </div>
+          )}
+        </form>
       ) : (
-        <button
-          type="button"
-          className="btn btn--secondary submit"
-          onClick={() => window.location.reload()}
-        >
-          Try Again
-        </button>
+        <p>loading</p>
       )}
-      {quizOver && (
-        <div className="score">You scored {points} points out of 5 points!</div>
-      )}
-    </form>
+    </>
   );
 }
